4f8286c6edcc7d8a8e118bc69e2196b337d8282d
KanniSuresh <kannisuresh3@gmail.com>
2022-12-25 19:41:06 +0530

    adding

---
+++ bank.py
@@ -0,0 +1,45 @@
+class AxisBank:
+    bank_na = "AXIS bank"
+    branch = "chennai"
+    pincode = 600094
+
+class Customer(AxisBank):
+    def __init__(self,firstname,lastname,accountno,balance):
+        self.lname = firstname
+        self.fname = lastname
+        self.acno = accountno
+        self.bal = balance
+
+    def details(self):
+        print(f"branch name:{self.branch}")
+        print(f"pincode:{self.pincode}")
+        print(f"lastname:{self.lname}")
+        print(f"firstname:{self.fname}")
+        print(f"account number:{self.acno}")
+    
+    def select(self):
+        select = input("if you enter w or d:")
+        if select == "w":
+           print(self.withdraw())
+        else:
+            print(self.deposite())
+    def deposite(self):
+        amt = int(input("enter your deposite amount: "))
+        self.bal += amt
+        print(f"dear {self.bank_na} customer yor deposite is successful")
+        print(f"{self.fname} your curent balance is:{self.bal}")
+    
+    def withdraw(self):
+        amt = int(input("enter your withdraw amount: "))
+        if self.bal >= amt:
+            self.bal -= amt
+            print(f"dear {self.bank_na} user your withdraw is successfull")
+            print(f"{self.fname} your current balance is:{self.bal}")
+        else:
+            print("insuffient balance")
+        return
+
+
+modi = Customer("Modi","kesav",1555123,5000)
+modi.details()
+modi.select()
